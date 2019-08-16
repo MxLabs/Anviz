9d953a691bdb8d7d29284146470bcfcc57e1ec12
Gianni Angelozzi <gianni@sistema3.it>
2019-08-16 16:46:34 +0200

    add missing fields

--- Anviz.SDK/Responses/UserInfo.cs
+++ Anviz.SDK/Responses/UserInfo.cs
@@ -5,17 +5,35 @@ namespace Anviz.SDK.Responses
     public class UserInfo
     {
         public ulong Id { get; }
+        public byte[] PWD { get; }
+        public byte[] Card { get; }
         public string Name { get; }
+        public byte Department { get; }
+        public byte Group { get; }
+        public byte Mode { get; }
+        public byte[] FP { get; }
+        public byte PWDH8 { get; }
+        public byte Keep { get; }
+        public byte Message { get; }
 
         public UserInfo(byte[] data, int offset)
         {
             Id = Bytes.Read(Bytes.Split(data, offset, 5));
+            PWD = Bytes.Split(data, offset + 6, 3);
+            Card = Bytes.Split(data, offset + 9, 3);
             Name = Bytes.GetUnicodeString(Bytes.Split(data, offset + 12, 20));
+            Department = data[ offset + 32];
+            Group = data[ offset + 33];
+            Mode = data[offset + 34];
+            FP = Bytes.Split(data, offset + 35, 2);
+            PWDH8 = data[offset + 37];
+            Keep = data[offset + 38];
+            Message = data[offset + 39];
         }
 
         public override string ToString()
         {
-            return string.Format("Id : {0}\r\nName : {1}", Id, Name);
+            return $"Id: {Id}\r\nName: {Name}";
         }
     }
 }
