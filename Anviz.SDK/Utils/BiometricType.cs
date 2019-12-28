6da5450b473f5950de251faeda783392f2eed579
Gianni Angelozzi <gianni@sistema3.it>
2019-12-28 11:00:44 +0100

    add W1

--- Anviz.SDK/Utils/BiometricType.cs
+++ Anviz.SDK/Utils/BiometricType.cs
@@ -16,6 +16,7 @@
             {
                 case "FACE7": //FACEPASS7
                     return BiometricType.Face;
+                case "W1": //W1 (PRO)
                 case "TC-B-N": //TC550
                     return BiometricType.Finger;
                 default:
