ffcfd39d7a2758f0ab31bc436294dd2832e6f3dd
Gianni Angelozzi <gianni@sistema3.it>
2020-09-25 18:50:17 +0200

    use index instead of constants

--- Anviz.SDK/Commands/Command.cs
+++ Anviz.SDK/Commands/Command.cs
@@ -28,19 +28,22 @@ namespace Anviz.SDK.Commands
              *     throw new Exception("Payload too big");
              * }
              */
-            Payload = new byte[8 + dataLength + 2]; //preamble + data + crc
-            Payload[0] = 0xA5;
-            Payload[1] = (byte)((DeviceId >> 24) % 256);
-            Payload[2] = (byte)((DeviceId >> 16) % 256);
-            Payload[3] = (byte)((DeviceId >> 8) % 256);
-            Payload[4] = (byte)(DeviceId % 256);
-            Payload[5] = command;
-            Payload[6] = (byte)(dataLength >> 8);
-            Payload[7] = (byte)(dataLength % 256);
-            data.CopyTo(Payload, 8);
+            var payloadLength = 8 + dataLength + 2; //preamble + data + crc
+            var i = 0;
+            Payload = new byte[payloadLength];
+            Payload[i++] = 0xA5;
+            Payload[i++] = (byte)((DeviceId >> 24) % 256);
+            Payload[i++] = (byte)((DeviceId >> 16) % 256);
+            Payload[i++] = (byte)((DeviceId >> 8) % 256);
+            Payload[i++] = (byte)(DeviceId % 256);
+            Payload[i++] = command;
+            Payload[i++] = (byte)(dataLength >> 8);
+            Payload[i++] = (byte)(dataLength % 256);
+            data.CopyTo(Payload, i);
+            i += data.Length;
             var crc = CRC16.Compute(Payload, Payload.Length - 2); //last 2 bytes = crc
-            Payload[8 + dataLength] = (byte)(crc % 256);
-            Payload[9 + dataLength] = (byte)((crc >> 8) % 256);
+            Payload[i++] = (byte)(crc % 256);
+            Payload[i++] = (byte)((crc >> 8) % 256);
         }
 
         public async Task Send(NetworkStream stream)
